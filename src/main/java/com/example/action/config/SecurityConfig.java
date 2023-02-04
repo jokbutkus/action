602a87cff0eeb73241b26aa84cb1a6a9fef46dec
Jokubas Butkus <jok.butkus@gmail.com>
2023-02-04 18:29:53 +0200

    Initial security config

---
+++ src/main/java/com/example/action/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.action.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/secure").denyAll()
+                .antMatchers("/", "/**").permitAll();
+        return http.build();
+    }
+}
--- src/main/java/com/example/action/controller/HomeController.java
+++ src/main/java/com/example/action/controller/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String home() {
         return "home";
     }
+
+    @GetMapping("/secure")
+    public String secure() {
+        return "secure";
+    }
 }
